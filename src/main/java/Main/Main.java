62b4c22291b0bcbf680f219d4209e3ed1bfbdeb0
karoll1naa <karollinnajan@gmail.com>
2024-10-08 06:37:36 +0300

    Adding an example to test the code

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -2,6 +2,8 @@ package Main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        StringBuffer text = new StringBuffer("Today is such a wonderful day. Oh no, today is the last day to pass the laboratory on operating systems. Well, then I will cry and sit down to make code in my favorite c++.");
+        StringOperation op = new StringOperation();
+        op.sortSentences(text);
     }
 }
